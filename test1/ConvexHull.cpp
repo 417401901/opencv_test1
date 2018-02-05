a6b193332509744aeecd8c1eeccb3e8974a82cee
417401901 <wx1998@hotmail.com>
2018-02-05 22:42:04 +0800

    寻找凸包缺陷

--- test1/ConvexHull.cpp
+++ test1/ConvexHull.cpp
@@ -25,14 +25,30 @@ void mainConvex(cv::InputArray mat)
 	vector<Vec4i> Hierarchy;
 	findContours(dst, Contours, Hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 	vector<vector<Point>> hull(Contours.size());
+	vector<vector<int>> hulls(Contours.size());
+	vector<vector<Vec4i>> defects(Contours.size());
 	for (int i = 0; i < Contours.size(); i++)
 	{
 		convexHull(Mat(Contours[i]), hull[i], false);
+		convexHull(Mat(Contours[i]), hulls[i], false);
+		convexityDefects(Mat(Contours[i]), hulls[i], defects[i]);
 	}
 	for (int i = 0; i < Contours.size(); i++)
 	{
-		drawContours(srcImage, Contours, i, Scalar(255, 0, 0), 3, 8);
-		drawContours(srcImage, hull, i, Scalar(0, 255, 255), 3, 8);
+		drawContours(srcImage, Contours, i, Scalar(255, 0, 0), 1, 8);
+		drawContours(srcImage, hull, i, Scalar(0, 255, 255), 1, 8);
+		vector<Vec4i> ::iterator d = defects[i].begin();
+		while (d != defects[i].end())
+		{
+			Vec4i &hh = (*d);
+			Point start = Contours[i][hh[0]];
+			Point end = Contours[i][hh[1]];
+			Point far = Contours[i][hh[2]];
+			line(srcImage, start, far, Scalar(0, 0, 255), 2, 8);
+			line(srcImage, far, end, Scalar(0, 0, 255), 2, 8);
+			circle(srcImage, start, 4, Scalar(0, 255, 0), 1, 8);
+			d++;
+		}
 	}
 	imshow("dst", srcImage);
 	waitKey();
